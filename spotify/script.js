2fc5fcd8a905d5f3b03bc001e46ad66c10ff45b4
Shubham Bansal <159136423+Shubham-py404@users.noreply.github.com>
2024-09-22 13:38:50 +0530

    Update script.js

--- spotify/script.js
+++ spotify/script.js
@@ -17,7 +17,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder= folder
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`https://shubham-py404.github.io/WEB-HOSTING/spotify/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
